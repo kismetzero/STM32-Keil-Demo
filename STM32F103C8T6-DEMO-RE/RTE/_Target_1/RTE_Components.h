21e48e8f2af9836de2e7efeed308a1bfb06d5a41
kismetzero <kismetzero@qq.com>
2026-04-18 12:08:01 +0800

    [更新] 添加keil5包管理驱动文件夹RTE

---
+++ STM32F103C8T6-DEMO-RE/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,45 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F103C8T6' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/* ARM.FreeRTOS::RTOS:Config:FreeRTOS:11.2.0 */
+#define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
+/* ARM.FreeRTOS::RTOS:Core:Cortex-M:11.2.0 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/* ARM.FreeRTOS::RTOS:Heap:Heap_4:11.2.0 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/* ARM.FreeRTOS::RTOS:Timers:11.2.0 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/* Keil::Device:StdPeriph Drivers:EXTI:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_EXTI
+/* Keil::Device:StdPeriph Drivers:Framework:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+/* Keil::Device:StdPeriph Drivers:GPIO:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_GPIO
+/* Keil::Device:StdPeriph Drivers:I2C:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_I2C
+/* Keil::Device:StdPeriph Drivers:RCC:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_RCC
+/* Keil::Device:StdPeriph Drivers:SPI:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_SPI
+/* Keil::Device:StdPeriph Drivers:TIM:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_TIM
+/* Keil::Device:StdPeriph Drivers:USART:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_USART
+
+
+#endif /* RTE_COMPONENTS_H */
